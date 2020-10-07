2835762166455260cfcbca7c266c9caf1af428db
Tomer Ronen <tomer.ronen34@gmail.com>
2020-10-07 15:18:59 +0300

    split Procfile into minimal Procfile and organized bash file

---
+++ heroku_run.sh
@@ -0,0 +1,8 @@
+cd ton_soundboard
+python generate_css_and_js_code_for_audio_buttons.py
+
+cd ..
+bokeh serve \
+    --allow-websocket-origin=ton-soundboard.herokuapp.com --address=0.0.0.0 --use-xheaders \
+    --port=$PORT \
+    ton_soundboard
