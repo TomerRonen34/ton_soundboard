736e73ddea7d9e3f1533fc4bcce5ad042f74df0f
Tomer Ronen <tomer.ronen34@gmail.com>
2020-10-07 20:31:42 +0300

    adjusted volume of snippets

--- run.bat
+++ run.bat
@@ -3,9 +3,6 @@ IF "%PORT%"=="" (
     SET PORT=2000
 )
 
-cd ton_soundboard
-python scripts/generate_css_and_js_code_for_audio_buttons.py
+python ton_soundboard/scripts/generate_css_and_js_code_for_audio_buttons.py
 
-cd ..
 bokeh serve --show ton_soundboard --port=%PORT%
-
--- ton_soundboard/scripts/generate_css_and_js_code_for_audio_buttons.py
+++ ton_soundboard/scripts/generate_css_and_js_code_for_audio_buttons.py
@@ -1,10 +1,16 @@
+import sys
+from pathlib import Path
+
+_SOURCES_ROOT = Path(__file__).absolute().parent.parent
+sys.path.append(str(_SOURCES_ROOT))
+
 from bokeh_layouts.button_props import generate_css_styles_file
 from bokeh_layouts.button_props import generate_js_file_for_audio_preloading
-from config import SOURCES_ROOT
+from config import SOURCES_ROOT, BUTTON_FILES_MASTER_DIR
 
 
 def main():
-    button_files_dir = SOURCES_ROOT / "static" / "button_files"
+    button_files_dir = BUTTON_FILES_MASTER_DIR
     css_styles_path = SOURCES_ROOT / "templates" / "audio_button_styles.css"
     audio_preloading_javascript_path = SOURCES_ROOT / "templates" / "audio_preloading.js"
 
